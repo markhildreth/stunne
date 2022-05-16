d04b1147571dcb79e48ecfae3d87fe0b82dfbfe2
Mark Hildreth <mark.k.hildreth@gmail.com>
2022-05-16 16:44:39 -0400

    Implemented more error cases.

--- stunne-client/src/protocol.rs
+++ stunne-client/src/protocol.rs
@@ -4,7 +4,7 @@ static BINDING_REQUEST: [u8; 2] = [0x00, 0x01];
 
 /// Magic data that must be included in all STUN messages to clarify that the STUN message
 /// uses rfc5389, rather than the outdated rfc3489.
-static STUN_MAGIC: [u8; 4] = [0x21, 0x12, 0xA4, 0x42];
+static MAGIC_COOKIE: [u8; 4] = [0x21, 0x12, 0xA4, 0x42];
 
 /// The class for a given STUN message.
 #[derive(Debug)]
@@ -24,7 +24,7 @@ impl StunRequest {
         // Crreate the header
         let mut buf = [0u8; 20];
         buf[0..2].copy_from_slice(&BINDING_REQUEST);
-        buf[4..8].copy_from_slice(&STUN_MAGIC);
+        buf[4..8].copy_from_slice(&MAGIC_COOKIE);
 
         // Generate the transaction ID randomly
         let mut tx_id = [0u8; 12];
@@ -41,12 +41,19 @@ impl StunRequest {
 
 #[derive(Debug)]
 pub enum HeaderParseError {
+    /// Every STUN header must start with two zero bits. This error is raised if either of those
+    /// two bits are set.
+    NonZeroStartingBits,
+
     /// The byte slice that the header was to be parsed from was too small.
     ///
     /// Each STUN header is 20 bytes. Therefore, attempting to read bytes from a
     /// slice less than 20 bytes in size will never succeed, and instead we will
     /// fail with this error.
     InvalidSize,
+
+    /// The magic cookie field did not have the fixed value of 0x2112A442.
+    InvalidMagicCookie,
 }
 
 #[derive(Debug)]
@@ -60,9 +67,19 @@ pub struct StunHeader {
 const STUN_HEADER_SIZE: usize = 20;
 
 impl StunHeader {
+    // NOTE: This bit manipulation was a quick first attempt. More performant manipulations
+    // are likely.
     pub fn from_bytes(buf: &[u8]) -> Result<Self, HeaderParseError> {
         if buf.len() < STUN_HEADER_SIZE {
             return Err(HeaderParseError::InvalidSize);
+        }
+
+        if (u8::from_be_bytes(buf[0..1].try_into().unwrap()) & 0b1100_0000) != 0 {
+            return Err(HeaderParseError::NonZeroStartingBits);
+        }
+
+        if buf[4..8] != MAGIC_COOKIE {
+            return Err(HeaderParseError::InvalidMagicCookie);
         }
 
         let (message_type_bytes, rest) = buf.split_at(std::mem::size_of::<u16>());
@@ -75,24 +92,21 @@ impl StunHeader {
         // Thus, some strange bit manipulation must be done.
         let message_type = u16::from_be_bytes(message_type_bytes.try_into().unwrap());
 
-        // NOTE: This bit manipulation was a quick first attempt. More performant manipulations
-        // are likely.
-        let class_bit_one = (message_type & 0b00000000100000000) >> 7;
-        let class_bit_two = (message_type & 0b00000000000010000) >> 4;
+        let class_bit_one = (message_type & 0b0000_0000_0001_0000_0000) >> 7;
+        let class_bit_two = (message_type & 0b0000_0000_0000_0001_0000) >> 4;
         let class = match class_bit_one | class_bit_two {
             0b00 => StunClass::Request,
             0b01 => StunClass::Indication,
             0b10 => StunClass::SuccessResponse,
             0b11 => StunClass::ErrorResponse,
-            // TODO: Proper error handling
             _ => {
-                todo!()
+                unreachable!()
             }
         };
 
-        let message_type_first_bits = message_type & 0b0000000000001111;
-        let message_type_second_bits = (message_type & 0b0000000011100000) >> 1;
-        let message_type_third_bits = (message_type & 0b0011111000000000) >> 2;
+        let message_type_first_bits = message_type & 0b0000_0000_0000_1111;
+        let message_type_second_bits = (message_type & 0b0000_0000_1110_0000) >> 1;
+        let message_type_third_bits = (message_type & 0b0011_1110_0000_0000) >> 2;
         let message_type =
             message_type_first_bits | message_type_second_bits | message_type_third_bits;
 
@@ -112,9 +126,66 @@ mod tests {
     use super::*;
 
     #[test]
-    fn invalid_size() {
+    fn test_invalid_size() {
         let result = StunHeader::from_bytes(&[0, 0]);
         assert!(matches!(result, Err(HeaderParseError::InvalidSize)));
+    }
+
+    #[test]
+    fn test_non_zero_msbs() {
+        #[rustfmt::skip]
+        let result = StunHeader::from_bytes(&[
+            0b1000_0000, 0, // Zero-padding & Message Type
+            0, 0, // Message Length
+            0x21, 0x12, 0xA4, 0x42, // Cookie
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
+        ]);
+        assert!(matches!(result, Err(HeaderParseError::NonZeroStartingBits)));
+
+        #[rustfmt::skip]
+        let result = StunHeader::from_bytes(&[
+            0b0100_0000, 0, // Zero-padding & Message Type
+            0, 0, // Message Length
+            0x21, 0x12, 0xA4, 0x42, // Cookie
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
+        ]);
+        assert!(matches!(result, Err(HeaderParseError::NonZeroStartingBits)));
+
+        #[rustfmt::skip]
+        let result = StunHeader::from_bytes(&[
+            0b1100_0000, 0, // Zero-padding & Message Type
+            0, 0, // Message Length
+            0x21, 0x12, 0xA4, 0x42, // Cookie
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
+        ]);
+        assert!(matches!(result, Err(HeaderParseError::NonZeroStartingBits)));
+    }
+
+    #[test]
+    fn test_invalid_magic_cookie() {
+        let result = StunHeader::from_bytes(&[
+            0, 0, // Zero-padding & Message Type
+            0, 0, // Message Length
+            0x20, 0x12, 0xA4, 0x42, // Cookie
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
+        ]);
+        assert!(matches!(result, Err(HeaderParseError::InvalidMagicCookie)));
+
+        let result = StunHeader::from_bytes(&[
+            0, 0, // Zero-padding & Message Type
+            0, 0, // Message Length
+            0x21, 0x12, 0xA4, 0x41, // Cookie
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
+        ]);
+        assert!(matches!(result, Err(HeaderParseError::InvalidMagicCookie)));
+
+        let result = StunHeader::from_bytes(&[
+            0, 0, // Zero-padding & Message Type
+            0, 0, // Message Length
+            0x21, 0x12, 0xB4, 0x42, // Cookie
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
+        ]);
+        assert!(matches!(result, Err(HeaderParseError::InvalidMagicCookie)));
     }
 
     #[test]
@@ -122,7 +193,7 @@ mod tests {
         let header = StunHeader::from_bytes(&[
             0, 0, // Zero-padding & Message Type
             0, 0, // Message Length
-            0, 0, 0, 0, // Cookie
+            0x21, 0x12, 0xA4, 0x42, // Cookie
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
         ])
         .unwrap();
@@ -133,10 +204,11 @@ mod tests {
 
     #[test]
     fn test_message_length() {
+        #[rustfmt::skip]
         let header = StunHeader::from_bytes(&[
             0, 0, // Zero-padding & Message Type
-            0b10000000, 0b00000001, // Message Length
-            0, 0, 0, 0, // Cookie
+            0b1000_0000, 0b0000_0001, // Message Length
+            0x21, 0x12, 0xA4, 0x42, // Cookie
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
         ])
         .unwrap();
@@ -145,10 +217,11 @@ mod tests {
 
     #[test]
     fn test_class_indication() {
+        #[rustfmt::skip]
         let header = StunHeader::from_bytes(&[
-            0b00000000, 0b00010000, // Zero-padding & Message Type
+            0b0000_0000, 0b0001_0000, // Zero-padding & Message Type
             0, 0, // Message Length
-            0, 0, 0, 0, // Cookie
+            0x21, 0x12, 0xA4, 0x42, // Cookie
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
         ])
         .unwrap();
@@ -157,10 +230,11 @@ mod tests {
 
     #[test]
     fn test_class_success_response() {
+        #[rustfmt::skip]
         let header = StunHeader::from_bytes(&[
-            0b00000001, 0b00000000, // Zero-padding & Message Type
+            0b0000_0001, 0b0000_0000, // Zero-padding & Message Type
             0, 0, // Message Length
-            0, 0, 0, 0, // Cookie
+            0x21, 0x12, 0xA4, 0x42, // Cookie
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
         ])
         .unwrap();
@@ -169,10 +243,11 @@ mod tests {
 
     #[test]
     fn test_class_error_response() {
+        #[rustfmt::skip]
         let header = StunHeader::from_bytes(&[
-            0b00000001, 0b00010000, // Zero-padding & Message Type
+            0b0000_0001, 0b0001_0000, // Zero-padding & Message Type
             0, 0, // Message Length
-            0, 0, 0, 0, // Cookie
+            0x21, 0x12, 0xA4, 0x42, // Cookie
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
         ])
         .unwrap();
@@ -181,25 +256,27 @@ mod tests {
 
     #[test]
     fn test_max_message_type() {
+        #[rustfmt::skip]
         let header = StunHeader::from_bytes(&[
-            0b00111110, 0b11101111, // Zero-padding & Message Type
+            0b0011_1110, 0b1110_1111, // Zero-padding & Message Type
             0, 0, // Message Length
-            0, 0, 0, 0, // Cookie
+            0x21, 0x12, 0xA4, 0x42, // Cookie
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
         ])
         .unwrap();
-        assert_eq!(header.message_type, 0b0000111111111111);
+        assert_eq!(header.message_type, 0b0000_1111_1111_1111);
     }
 
     #[test]
     fn test_message_type() {
+        #[rustfmt::skip]
         let header = StunHeader::from_bytes(&[
-            0b00011110, 0b10101101, // Zero-padding & Message Type
+            0b0001_1110, 0b1010_1101, // Zero-padding & Message Type
             0, 0, // Message Length
-            0, 0, 0, 0, // Cookie
+            0x21, 0x12, 0xA4, 0x42, // Cookie
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Transaction ID
         ])
         .unwrap();
-        assert_eq!(header.message_type, 0b0000011111011101);
+        assert_eq!(header.message_type, 0b0000_0111_1101_1101);
     }
 }
